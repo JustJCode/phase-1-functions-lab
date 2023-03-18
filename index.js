7fa4ed556e9539c88abc415a286834b28ade15be
Julian Victorio <julianv714@gmail.com>
2023-03-18 01:34:27 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks < 42){
+        return 42 -blocks
+    }
+    return (blocks - 42)
+}
+
+function distanceFromHqInFeet(feet){
+    return (distanceFromHqInBlocks(feet) * 264)
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination - start) * 264
+    }
+    return (start-destination) * 264
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = (start - destination) * 264
+
+    if (feet < 0 && feet > -400) {
+        return (feet * -1) + feet
+    } 
+    else if (feet > 0 && feet < 2499) {
+        return (feet - 400) * .02
+    }
+    else if (feet < 0 && feet < -400) {
+        return ((feet * -1) + feet) + 25
+    }
+    else if (feet > 2500) {
+        return 'cannot travel that far'
+    }
+ 
+}
